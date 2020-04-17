0b4d5630fafee01c485f58518da386e11bcc40bb
Vadim Rybakov <vadimio9217@gmail.com>
2020-04-17 09:14:36 +0300

    load spring boot starter

---
+++ src/test/java/ru/mycreation/MycreationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mycreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
